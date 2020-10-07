9850d53bda50422353689736b5413d009846dcb6
fbottarel <fabrizio.bottarel@iit.it>
2020-10-07 20:47:56 +0200

    added modification of the ACM in the setup code. Not sure it works  though...

--- src/panda_grasp_server/panda_grasp_server/simple_action_server.py
+++ src/panda_grasp_server/panda_grasp_server/simple_action_server.py
@@ -233,6 +233,31 @@ class PandaActionServer(object):
         workbench_pose.pose.position.z = - 0.1 - config._bench_dimensions[2]/2
         self._scene.add_box("workbench", workbench_pose, config._bench_dimensions)
 
+        # Turn off collisions between link_0 and workbench
+        #TODO: rewrite this!
+        from moveit_msgs.msg import PlanningScene, PlanningSceneComponents
+        from moveit_msgs.srv import GetPlanningScene
+        pub_planning_scene = rospy.Publisher('/planning_scene', PlanningScene, queue_size=10)
+        rospy.wait_for_service('/get_planning_scene', 10.0)
+        get_planning_scene = rospy.ServiceProxy('/get_planning_scene', GetPlanningScene)
+        request = PlanningSceneComponents(components=PlanningSceneComponents.ALLOWED_COLLISION_MATRIX)
+        response = get_planning_scene(request)
+
+        acm = response.scene.allowed_collision_matrix
+        if not 'workbench' in acm.default_entry_names:
+            # add button to allowed collision matrix
+            acm.default_entry_names += ['workbench']
+            acm.default_entry_values += [True]
+
+            planning_scene_diff = PlanningScene(
+                    is_diff=True,
+                    allowed_collision_matrix=acm)
+
+            pub_planning_scene.publish(planning_scene_diff)
+            rospy.sleep(1.0) 
+
+
+
 
     def set_home(self, pos, quat):
 
