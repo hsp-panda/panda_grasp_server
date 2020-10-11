d0654a5a8941f341545be3a3ae73c90bbfb51a27
fbottarel <fabrizio.bottarel@iit.it>
2020-10-11 02:44:15 +0200

    added code to generate calibration poses

--- src/panda_grasp_server/panda_grasp_server/calib.py
+++ src/panda_grasp_server/panda_grasp_server/calib.py
@@ -1,32 +1,71 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from tf.transformations import quaternion_from_matrix
+from geometry_msgs.msg import Pose
 
-theta=np.random.random_sample((1, 100))*2*np.pi
-phi=np.reshapenp.random.random_sample((1, 100))*np.pi/2
-rad=0.5
+def normalize_rows(mat_to_normalize):
+    sum_of_rows = np.sum(mat_to_normalize, axis=1)
+    return mat_to_normalize / sum_of_rows[:, np.newaxis]
 
-center = [0.5, 0, 0]
+# Define ranges of theta and phi
+theta = np.linspace(np.pi/2, 3/2*np.pi, 10)
+phi = np.linspace(0, np.pi/2, 10)
+rad = 0.5
 
-x = rad*np.cos(theta)*np.sin(phi) + center[0]
+# Organize the parameter grid
+theta_space = np.empty((0,))
+phi_space = np.empty((0,))
+for idx in range(phi.size):
+    theta_space = np.append(theta_space, theta)
+    phi_space = np.append(phi_space, phi[idx]*np.ones_like(theta))
 
-y = rad*np.sin(theta)*np.sin(phi) + center[1]
+theta_space = np.reshape(theta_space, (1, theta_space.size))
+phi_space = np.reshape(phi_space, (1, phi_space.size))
 
-z = rad*np.cos(phi) + center[2]
+# Define sphere center
+center = [2, 0, 0]
+
+# Obtain pose center points
+x = rad*np.cos(theta_space)*np.sin(phi_space) + center[0]
+y = rad*np.sin(theta_space)*np.sin(phi_space) + center[1]
+z = rad*np.cos(phi_space) + center[2]
 
 # find the x axis by looking at center of sphere
 # find the y axis by finding the derivative of x and y wrt theta
-# find the z axis by outer product 
+# find the z axis by cross product
 
-# rosrun tf static_transform_publisher 0.061251 -0.0397 -0.592961 0.002414 -1.6 3.1249 /panda_EE /camera_link 50
-
-points = np.hstack((x,y,z))
+points = np.hstack((np.transpose(x),np.transpose(y),np.transpose(z)))
 
 x_ax = np.reshape(center, (1,3)) - points
+x_ax = normalize_rows(x_ax)
 
-#y_ax = 
+y_ax_x = -rad*np.sin(theta_space)*np.sin(phi_space)
+y_ax_y = rad*np.cos(theta_space)*np.sin(phi_space)
+y_ax_z = np.zeros_like(y_ax_x)
 
-#z_ax = 
+y_ax = np.hstack((np.transpose(y_ax_x),np.transpose(y_ax_y),np.transpose(y_ax_z)))
+y_ax = normalize_rows(y_ax)
+
+z_ax = np.cross(x_ax, y_ax)
+z_ax = normalize_rows(z_ax)
+
+# Obtain poses as position + quaternion
+poses = []
+for idx in range(x_ax.shape[0]):
+    rot_matrix = np.identity(4)
+    rot_matrix[:3, :3] = np.transpose(np.stack((
+                                            x_ax[idx,:],
+                                            y_ax[idx,:],
+                                            z_ax[idx,:]
+                                        )))
+
+    pose_orient = quaternion_from_matrix(rot_matrix)
+    pose_origin = points[idx, :]
+    pose = Pose(
+                position = pose_origin,
+                orientation = pose_orient)
+
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -34,7 +73,6 @@ ax = fig.add_subplot(111, projection='3d')
 ax.scatter(x,y,z)
 
 plt.show()
-
 
 
 
